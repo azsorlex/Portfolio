9103f2387c4f03cb534bdaacdf19f9dbf1477f78
azsorlex <arozsa@protonmail.com>
2024-06-09 20:19:43 +1000

    Remove reference

--- Portfolio.WebClient/src/main.jsx
+++ Portfolio.WebClient/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import 'material-icons/iconfont/material-icons.css';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { ErrorBoundary } from 'react-error-boundary';
