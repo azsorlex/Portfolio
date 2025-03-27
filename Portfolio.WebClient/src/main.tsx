af27244810ed01a5b553f6d2fe2a30f5498f79e8
azsorlex <github.com.bricq@passmail.net>
2025-03-27 16:17:34 +1100

    Migrate to RouterProvider

--- Portfolio.WebClient/src/main.tsx
+++ Portfolio.WebClient/src/main.tsx
@@ -2,18 +2,20 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { ErrorBoundary } from 'react-error-boundary';
 
 history.scrollRestoration = "manual";
 
+const router = createBrowserRouter([
+    { path: "*", element: <App /> },
+]);
+
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <BrowserRouter>
-            <ErrorBoundary fallback={<div>Something went wrong</div>} onError={console.error}>
-                <App />
-            </ErrorBoundary>
-        </BrowserRouter>
+        <ErrorBoundary fallback={<div>Something went wrong</div>} onError={console.error}>
+            <RouterProvider router={router} />
+        </ErrorBoundary>
     </StrictMode>,
 )
