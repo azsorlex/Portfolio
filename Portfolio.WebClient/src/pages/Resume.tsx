b031f6d6fb16f3adbe701ec8e3799552eb26bb8c
azsorlex <164110507+azsorlex@users.noreply.github.com>
2025-08-17 11:31:55 +1000

    Update web packages and allow pdf multi-page rendering

--- Portfolio.WebClient/src/pages/Resume.tsx
+++ Portfolio.WebClient/src/pages/Resume.tsx
@@ -1,7 +1,7 @@
 import 'react-pdf/dist/Page/TextLayer.css';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import AnimatedMain from "../components/Shared/AnimatedMain";
-import { Button, Typography } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 import { useState } from "react";
 import { Document, Page, pdfjs } from 'react-pdf';
 import { saveAs } from 'file-saver';
@@ -10,10 +10,7 @@ import PageContainer from '../components/Shared/PageContainer';
 const url = "https://portfoliowebsite.blob.core.windows.net/public-files/Alexander%20Rozsa%20-%20Resume.pdf";
 
 export default function Resume() {
-    pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-        'pdfjs-dist/build/pdf.worker.min.mjs',
-        import.meta.url,
-    ).toString();
+    pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
     const [numPages, setNumPages] = useState<number>(0);
 
@@ -38,7 +35,15 @@ export default function Resume() {
                     className="ResumeViewer"
                     file={url}
                     onLoadSuccess={onDocumentLoad}>
-                    <Page pageNumber={numPages} scale={window.outerWidth > 786 ? 1.5 : 0.6} />
+                    {Array(numPages).keys().map((i) => (
+                        <Box
+                            component={Page}
+                            key={i}
+                            pageNumber={i + 1}
+                            scale={window.outerWidth > 786 ? 1.5 : 0.6}
+                            sx={{ mt: i > 0 ? 2 : 0 }}
+                        />
+                    ))}
                 </Document>
                 <Button
                     variant='outlined'
